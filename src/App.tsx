d52641e57f0e120a76c74a3feb15725b604cbfab
Marika <mashakonturova@gmail.com>
2022-01-07 19:44:12 +0300

    fix small issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,33 @@
-import React, {useState} from 'react';
-import './App.css';
-import {TodoTasks} from "./components/TodoTasks";
-import {TodoTask} from "./components/TodoTask";
-import {taskType} from "./context/Context";
+import React, { useState } from "react";
+import "./App.css";
+import { TodoTasks } from "./components/TodoTasks";
+import { TodoTask } from "./components/TodoTask";
+import { taskType } from "./context/Context";
 
 function App() {
-
-    const [activeTask, setActiveTask] = useState<taskType | null>(localStorage.getItem('task') ? JSON.parse(localStorage.getItem('task') as string) : null)
-    const onTaskChange = (task?: taskType) => {
-        if (task) {
-            localStorage.setItem('task', JSON.stringify(task))
-            setActiveTask(task)
-        } else {
-            localStorage.setItem('task', JSON.stringify(null))
-            setActiveTask(null)
-        }
-
+  const [activeTask, setActiveTask] = useState<taskType | null>(
+    localStorage.getItem("task")
+      ? JSON.parse(localStorage.getItem("task") as string)
+      : null
+  );
+  const onTaskChange = (task?: taskType) => {
+    if (task) {
+      localStorage.setItem("task", JSON.stringify(task));
+      setActiveTask(task);
+    } else {
+      localStorage.setItem("task", JSON.stringify(null));
+      setActiveTask(null);
     }
+  };
 
-    return (
-        <div className="App">
-
-            <div className={'container'}>
-                <div  className={'todotasks'}>
-                    <TodoTasks onTaskChange={onTaskChange}/>
-                </div>
-                <div className={'todotask'}>
-                    <TodoTask task={activeTask} onTaskChange={onTaskChange}/>
-                </div>
-            </div>
-        </div>
-    );
+  return (
+    <div className="App">
+      <div className={"container"}>
+        <TodoTasks onTaskChange={onTaskChange} />
+        <TodoTask task={activeTask} onTaskChange={onTaskChange} />
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/components/TodoTask.tsx
+++ src/components/TodoTask.tsx
@@ -1,84 +1,106 @@
-import React, {useContext, useState} from 'react'
-import {Context, statusType, taskType} from "../context/Context";
-import {deleteTask, updateTask} from "../context/Actions";
-import s from './Todotask.module.css'
+import React, { useContext, useState } from "react";
+import { Context, statusType, taskType } from "../context/Context";
+import { deleteTask, updateTask } from "../context/Actions";
+import s from "./Todotask.module.css";
 
 type TodoTaskType = {
-    task: taskType | null
-    onTaskChange: (task?: taskType) => void
-}
+  task: taskType | null;
+  onTaskChange: (task?: taskType) => void;
+};
 
-export function TodoTask({task, onTaskChange}: TodoTaskType) {
-    const [updateMode, setUpdateMode] = useState(false)
-    const {dispatch} = useContext(Context)
-    const onTdtDelete = (id: string) => {
-        dispatch(deleteTask(id))
-        onTaskChange();
+export function TodoTask({ task, onTaskChange }: TodoTaskType) {
+  const [updateMode, setUpdateMode] = useState(false);
+  const { dispatch } = useContext(Context);
+  const onTdtDelete = (id: string) => {
+    dispatch(deleteTask(id));
+    onTaskChange();
+  };
+  const handleSubmit = async (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    if (task) {
+      const target = e.target as typeof e.target & {
+        title: { value: string };
+        desc: { value: string };
+        status: { value: statusType };
+      };
+      const title = target.title.value;
+      const desc = target.desc.value;
+      const id = task.id;
+      const status = target.status.value;
+      let newTask: taskType = { id, title, desc, status };
+      dispatch(updateTask(newTask));
+      onTaskChange(newTask);
+      setUpdateMode(false);
+      console.log(newTask);
     }
-    const handleSubmit = async (e: React.SyntheticEvent) => {
-        e.preventDefault();
-        if (task) {
-            const target = e.target as typeof e.target & {
-                title: { value: string };
-                desc: { value: string };
-                status: { value: statusType }
-            };
-            const title = target.title.value;
-            const desc = target.desc.value;
-            const id = task.id
-            const status = target.status.value
-            let newTask: taskType = {id, title, desc, status}
-            dispatch(updateTask(newTask))
-            onTaskChange(newTask)
-            setUpdateMode(false)
-            console.log(newTask)
-        }
-    }
-    if (!task) {
-        return <div className={s.container}>
-            <p style={{color: 'rgb(117, 117, 117)', alignSelf: 'center'}}>Choose task or create one...</p>
+  };
+  if (!task) {
+    return (
+      <div className={s.container}>
+        <p style={{ color: "rgb(117, 117, 117)", alignSelf: "center" }}>
+          Choose task or create one...
+        </p>
+      </div>
+    );
+  }
+
+  if (updateMode) {
+    return (
+      <div className={s.scroll}>
+        <form
+          onSubmit={handleSubmit}
+          className={`${s.container} ${s.update_container}`}
+        >
+          <textarea
+            name={"title"}
+            defaultValue={task.title}
+            id={"textInput"}
+            className={s.textarea}
+            autoFocus
+          />
+
+          <select
+            name={"status"}
+            defaultValue={task.status}
+            className={s.button}
+          >
+            <option>pending</option>
+            <option>started</option>
+            <option>done</option>
+          </select>
+          <textarea
+            name={"desc"}
+            defaultValue={task.desc}
+            className={s.textarea}
+          />
+          <button className={`${s.button} ${s.update_button}`}>Update</button>
+        </form>
+      </div>
+    );
+  }
+  return (
+    <section className={s.container}>
+      <div className={s.scroll}>
+        <h1 className={s.h1}>{task.title}</h1>
+        <p
+          className={
+            (task.status === "pending" && `${s.pending}`) ||
+            (task.status === "started" && `${s.started}`) ||
+            `${s.done}`
+          }
+        >
+          status: {task.status}
+        </p>
+        <div className={s.buttons}>
+          <button className={s.button} onClick={() => setUpdateMode(true)}>
+            Update
+          </button>
+          <button className={s.button} onClick={() => onTdtDelete(task.id)}>
+            Delete
+          </button>
         </div>
-    }
-
-    if (updateMode) {
-        return <div className={s.scroll}>
-            <form onSubmit={handleSubmit} className={`${s.container} ${s.update_container}`}>
-                <textarea name={'title'}
-                          defaultValue={task.title} id={'textInput'} className={s.textarea} autoFocus/>
-
-                <select name={'status'} defaultValue={task.status} className={s.button}>
-                    <option>pending</option>
-                    <option>started</option>
-                    <option>done</option>
-                </select>
-                <div>
-                    <textarea name={'desc'} defaultValue={task.desc} className={s.textarea}/>
-                </div>
-                <button className={`${s.button} ${s.update_button}`}>Update</button>
-            </form>
-        </div>
-    }
-    return <div className={s.container}>
-        <div className={s.scroll}>
-            <h1 className={s.h1}>{task.title}</h1>
-
-            {/*first condition = pending
-                second cond = started
-                third cond = done*/}
-            <div
-                className={task.status !== 'done' ? task.status !== 'started' ? `${s.pending}` : `${s.started}` : `${s.done}`}>
-                status: {task.status}
-            </div>
-            <div className={s.buttons}>
-                <button className={s.button} onClick={() => setUpdateMode(true)}>Update</button>
-                <button className={s.button} onClick={() => onTdtDelete(task.id)}>Delete</button>
-            </div>
-
-            <div>
-                <p className={s.desc}>{task.desc}</p>
-            </div>
-        </div>
-    </div>
-
-
+        <p className={s.desc}>{task.desc}</p>
+      </div>
+    </section>
+  );
 }
